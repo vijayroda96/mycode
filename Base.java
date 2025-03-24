995177e6b9975d02920a748a5884f994495416f1
vijay <vijaygulabchand.roda@focalcxm.com>
2025-03-25 00:58:12 +0530

    Check for main

---
+++ Base.java
@@ -0,0 +1,10 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Base {
+
+    public static void main(String[] args) {
+         String c1="Vijay Gulabchand Roda";
+
+        System.out.println(c1);
+    }
+}
